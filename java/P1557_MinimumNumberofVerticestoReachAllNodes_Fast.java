99ff725ea1a522a29616c6aa193b2953435b764d
gbko <aleks.gbko@gmail.com>
2026-04-17 13:12:00 +0700

    chore: Add a java solution/s

---
+++ java/P1557_MinimumNumberofVerticestoReachAllNodes_Fast.java
@@ -0,0 +1,32 @@
+import java.util.*; // ArrayList, HashMap, HashSet, Queue, PriorityQueue, etc.
+import java.util.Arrays; // Arrays.sort(), Arrays.fill(), Arrays.asList()
+import java.util.Collections; // Collections.sort(), Collections.reverseOrder()
+
+// https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/description/
+
+class P1557_MinimumNumberofVerticestoReachAllNodes_Fast {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        // handle null
+        List<Integer> ans = new ArrayList<>();
+        if (edges.size() == 0 || n == 0) {
+            return ans;
+        }
+
+        // index -> node number
+        // value -> count of incoming edges into the node
+        int[] indegreeCount = new int[n];
+        // count how many incoming edges
+        for (List<Integer> edge : edges) {
+            indegreeCount[edge.get(1)]++;
+        }
+
+        // filter nodes with 0 incoming edges
+        for (int i = 0; i < n; i++) {
+            if (indegreeCount[i] == 0) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ java/P1557_MinimumNumberofVerticestoReachAllNodes_HashMapSlow.java
@@ -0,0 +1,34 @@
+import java.util.*; // ArrayList, HashMap, HashSet, Queue, PriorityQueue, etc.
+import java.util.Arrays; // Arrays.sort(), Arrays.fill(), Arrays.asList()
+import java.util.Collections; // Collections.sort(), Collections.reverseOrder()
+
+// https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/description/
+
+class P1557_MinimumNumberofVerticestoReachAllNodes_HashMapSlow {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        // handle null
+        List<Integer> ans = new ArrayList<>();
+        if (edges.size() == 0 || n == 0) {
+            return ans;
+        }
+
+        // save all FROM edges
+        Map<Integer, Boolean> mapOfFrom = new HashMap<>();
+        for (List<Integer> edge : edges) {
+            mapOfFrom.put(edge.get(0), true);
+        }
+
+        // input - edge list - nothing to transform to
+
+        // count the indegree nodes only
+        // indegree are nodes that don't have anything to pointing to them
+        // remove items from map if there are any TO edges pointing to it
+        for (List<Integer> edge : edges) {
+            mapOfFrom.remove(edge.get(1));
+        }
+
+        // time complexity is high, since we iterate twice
+
+        return new ArrayList<>(mapOfFrom.keySet());
+    }
+}
